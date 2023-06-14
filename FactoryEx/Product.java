4ab15dd24bef0acea5127ac27104dfa6e10867a2
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-06-14 13:32:16 +0300

    Update Product.java
    setted initial Interface as a  class Product

--- FactoryEx/Product.java
+++ FactoryEx/Product.java
@@ -1,6 +1,23 @@
-public interface Product{
-    public String getName();
-    public void setName(String name);
-    public double getPrice();
-    public void setPrice(double price);
+abstract class Product{
+    private String name;
+    private double price;
+    
+    public String getName() {
+       return name;
+    }
+
+    
+    public void setName(String name) {
+       this.name = name;
+    }  
+
+   
+    public double getPrice() {
+       return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
